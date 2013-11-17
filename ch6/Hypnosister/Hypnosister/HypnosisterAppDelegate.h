eebeac241a19052dd6b472060505d17b15dafd70
Richard Shin <skunkworks@gmail.com>
2013-11-17 09:29:37 -0800

    Initial commit for ch6

---
+++ ch6/Hypnosister/Hypnosister/HypnosisterAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HypnosisterAppDelegate.h
+//  Hypnosister
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HypnosisterAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ch6/Hypnosister/Hypnosister/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Hypnosister
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnosisterAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnosisterAppDelegate class]));
+    }
+}
