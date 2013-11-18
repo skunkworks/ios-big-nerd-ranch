0047105c41b1e8208598864eafdce8d23af3f56d
Richard Shin <skunkworks@gmail.com>
2013-11-17 16:06:14 -0800

    First crack at ch7

---
+++ ch7/HypnoTime/HypnoTime/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoTime
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HypnoAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HypnoAppDelegate class]));
+    }
+}
