6796304c6d4fcff89dcf12fc93a5b197d32b62ef
Richard Shin <skunkworks@gmail.com>
2013-11-17 18:59:56 -0800

    Initialized ch8

---
+++ ch8/HeavyRotation/HeavyRotation/RotationAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RotationAppDelegate.h
+//  HeavyRotation
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RotationAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
