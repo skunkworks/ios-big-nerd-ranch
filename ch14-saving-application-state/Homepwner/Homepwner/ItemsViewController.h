0020d4821d856c6c35d5a2025f568d6c91393e28
Richard Shin <skunkworks@gmail.com>
2013-11-19 12:53:34 -0800

    ch14 initial commit

---
+++ ch14-saving-application-state/Homepwner/Homepwner/ItemsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.h
+//  Homepwner
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ItemsViewController : UITableViewController
+
+@end
