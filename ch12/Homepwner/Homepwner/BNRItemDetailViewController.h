031f7d6346568d75a0ce607e46a8582c6295e25f
Richard Shin <skunkworks@gmail.com>
2013-11-18 20:51:17 -0800

    Ch 12 initial commit

---
+++ ch12/Homepwner/Homepwner/BNRItemDetailViewController.h
@@ -0,0 +1,16 @@
+//
+//  BNRItemDetailViewController.h
+//  Homepwner
+//
+//  Created by Richard Shin on 11/18/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "BNRItem.h"
+
+@interface BNRItemDetailViewController : UIViewController
+
+@property (nonatomic, strong) BNRItem *item;
+
+@end
---
+++ ch12/Homepwner/Homepwner/BNRItemStore.h
@@ -0,0 +1,25 @@
+//
+//  BNRItemStore.h
+//  Homepwner
+//
+//  Created by Richard Shin on 11/17/13.
+//  Copyright (c) 2013 Richard Shin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "BNRItem.h"
+
+@interface BNRItemStore : NSObject
+{
+    NSMutableArray *items;
+}
+
+// Singleton interface
++ (BNRItemStore *)sharedStore;
+
+- (NSArray *)allItems;
+- (BNRItem *)createItem;
+- (void)deleteItem:(BNRItem *)item;
+- (void)moveItemAtIndex:(NSUInteger)sourceIndex
+                toIndex:(NSUInteger)destinationIndex;
+@end
