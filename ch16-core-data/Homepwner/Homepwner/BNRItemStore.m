b52a6fcf41bf74daed1e15dcf8981c386d19780a
Richard Shin <skunkworks@gmail.com>
2014-01-07 16:03:34 -0800

    Converted from archiving to Core Data persistence

--- ch16-core-data/Homepwner/Homepwner/BNRItemStore.m
+++ ch16-core-data/Homepwner/Homepwner/BNRItemStore.m
@@ -7,9 +7,13 @@
 //
 
 #import "BNRItemStore.h"
+#import "BNRImageStore.h"
 
 @interface BNRItemStore ()
 @property (nonatomic, strong) NSMutableArray *items;
+@property (nonatomic, strong) NSMutableArray *assetTypes;
+@property (nonatomic, strong) NSManagedObjectContext *context;
+@property (nonatomic, strong) NSManagedObjectModel *model;
 @end
 
 @implementation BNRItemStore
@@ -30,13 +34,61 @@
 - (id)init
 {
     if (self = [super init]) {
-        NSString *path = [self itemArchivePath];
-        _items = [NSKeyedUnarchiver unarchiveObjectWithFile:path];
-        if (_items) {
-            _items = [[NSMutableArray alloc] init];
-        }
+        [self setUpCoreData];
+        [self loadAllItems];
     }
     return self;
+}
+
+- (void)setUpCoreData
+{
+    // Search main bundle for a CD model file, and create MOM from it
+    self.model = [NSManagedObjectModel mergedModelFromBundles:nil];
+
+    // Create PSC with MOM
+    NSPersistentStoreCoordinator *psc = [[NSPersistentStoreCoordinator alloc] initWithManagedObjectModel:self.model];
+    
+    // Get path to SQLite database file
+    NSString *path = [self itemArchivePath];
+    NSURL *storeURL = [NSURL fileURLWithPath:path];
+    
+    // Load PSC from SQLite database file, or create if it doens't exist
+    NSError *error;
+    if (![psc addPersistentStoreWithType:NSSQLiteStoreType
+                      configuration:nil
+                                URL:storeURL
+                            options:nil
+                              error:&error])
+    {
+        [NSException raise:@"Open failed"
+                    format:@"Reason: %@", [error localizedDescription]];
+    }
+    
+    // Create MOC, and set its PSC to the one we created
+    self.context = [[NSManagedObjectContext alloc] init];
+    self.context.persistentStoreCoordinator = psc;
+    
+    // We don't need an undo manager
+    self.context.undoManager = nil;
+    
+}
+
+// Fetch/load all items from SQLite database
+- (void)loadAllItems
+{
+    NSFetchRequest *req = [NSFetchRequest fetchRequestWithEntityName:@"BNRItem"];
+    
+    NSSortDescriptor *sd = [NSSortDescriptor sortDescriptorWithKey:@"orderingValue" ascending:YES];
+    req.sortDescriptors = @[sd];
+    
+    NSError *error;
+    NSArray *fetchResults = [self.context executeFetchRequest:req error:&error];
+    if (!fetchResults) {
+        [NSException raise:@"Fetch failed"
+                    format:@"Reason: %@", [error localizedDescription]];
+    }
+    
+    self.items = [[NSMutableArray alloc] initWithArray:fetchResults];
 }
 
 - (NSArray *)allItems
@@ -46,13 +98,21 @@
 
 - (BNRItem *)createItem
 {
-    BNRItem *item = [[BNRItem alloc] init];
+    BNRItem *item = [NSEntityDescription insertNewObjectForEntityForName:@"BNRItem"
+                                                  inManagedObjectContext:self.context];
+    
+    double order = [self.items count] ? [[self.items lastObject] orderingValue] + 1.0 : 1.0;
+    item.orderingValue = order;
+
     [self.items addObject:item];
     return item;
 }
 
 - (void)deleteItem:(BNRItem *)item
 {
+    [self.context deleteObject:item];
+    // Oops! Forgot to add this line in previous versions to delete cached image file
+    [[BNRImageStore sharedStore] deleteImageForKey:item.imageKey];
     [self.items removeObjectIdenticalTo:item];
 }
 
@@ -66,8 +126,12 @@
 
 - (BOOL)saveItems
 {
-    NSString *path = [self itemArchivePath];
-    return [NSKeyedArchiver archiveRootObject:self.items toFile:path];
+    NSError *error;
+    BOOL successful = [self.context save:&error];
+    if (!successful) {
+        NSLog(@"Error saving: %@", [error localizedDescription]);
+    }
+    return successful;
 }
 
 // Book has us declare this in header file, but why does anyone else other
@@ -79,7 +143,7 @@
                                                   inDomains:NSUserDomainMask];
     NSURL *url = [docDirectories objectAtIndex:0];
 
-    return [[url URLByAppendingPathComponent:@"items.archive"] path];
+    return [[url URLByAppendingPathComponent:@"store.data"] path];
 }
 
 @end
