b0aec1e6c9171bafa6580c1912928ab782c27e2a
Richard Shin <skunkworks@gmail.com>
2014-01-07 16:04:02 -0800

    Added some wizardry to ignore compiler warning on performSelector

--- ch16-core-data/Homepwner/Homepwner/HomepwnerItemCell.m
+++ ch16-core-data/Homepwner/Homepwner/HomepwnerItemCell.m
@@ -16,13 +16,19 @@
     selector = [selector stringByAppendingString:@"atIndexPath:"];
     
     SEL newSelector = NSSelectorFromString(selector);
+    IMP imp = [self.controller methodForSelector:newSelector];
+    void (*func)(id, SEL) = (void *)imp;
+    func(self.controller, newSelector);
     
     NSIndexPath *indexPath = [self.tableView indexPathForCell:self];
     if (!indexPath) return;
     if ([self.controller respondsToSelector:newSelector]) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Warc-performSelector-leaks"
         [self.controller performSelector:newSelector
                               withObject:sender
                               withObject:indexPath];
+#pragma clang diagnostic pop
     }
 }
 
